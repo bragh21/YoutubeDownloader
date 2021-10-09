d6d3de92f58c04b293ab39020b44b349c322c48d
bragh21 <bragherolli23@gmail.com>
2021-10-09 14:48:43 -0300

    .

--- downloader.bat
+++ downloader.bat
@@ -3,7 +3,7 @@ color 30
 
 set baseDir=%~dp0
 set envPython="%baseDir:"=%.venv\Scripts\python.exe"
-set script="%baseDir:"=%main.py"
+set script="%baseDir:"=%mainv0.2.py"
 
 %envPython% %script%
 pause
